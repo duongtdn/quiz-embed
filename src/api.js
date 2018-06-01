6a16332047bfa5f1ba76c02d7a29a636f4dc9dd7
duongtdn <duongtdn@gmail.com>
2018-06-01 09:26:17 +0700

    change default url

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 "use strict"
 
-const SRC = 'http://localhost:3200/quiz/index.html';
+const SRC = 'http://localhost:3000/quiz/index.html';
 
-const QUIZ_ORIGIN = 'http://localhost:3200';
+const QUIZ_ORIGIN = 'http://localhost:3000';
 
 export default class Player {
   constructor(playerId, { playerVars, events }) {
